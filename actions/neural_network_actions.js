3f70653873f73e894c09372032e68214a3d7182e
Patrick W <patricw2@uci.edu>
2018-06-23 19:38:59 -0700

    Add basic header

--- actions/neural_network_actions.js
+++ actions/neural_network_actions.js
@@ -1,5 +1,8 @@
 export const RECEIVE_NEURAL_NETWORK = "RECEIVE_NEURAL_NETWORK";
 
+
+// dispatch an empty neural network to the reducer for the user to train in local storage.  
+// should not modify our existing neural network that is trained.
 export const receiveNeuralNetwork = net => ({
     type: RECEIVE_NEURAL_NETWORK,
     net
