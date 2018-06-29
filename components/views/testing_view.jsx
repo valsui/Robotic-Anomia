eb2638657fcfa73a91acfddabdc08f45f3e245b5
Patrick W <patricw2@uci.edu>
2018-06-28 17:57:18 -0700

    Add navigation

--- components/views/testing_view.jsx
+++ components/views/testing_view.jsx
@@ -2,10 +2,19 @@ import React from 'react';
 import { connect } from 'react-redux';
 import TestingCanvas from '../canvas/testing_canvas';
 import OutputContainer from '../displays/outputs_container';
+import { selectTrainedNetwork } from '../../actions/neural_network_actions';
 
 class TestingView extends React.Component {
     constructor(props) {
         super(props);
+    }
+
+    componentDidMount() {
+        const { currentNetwork, selectTrainedNet } = this.props;
+
+        if ( currentNetwork === null || currentNetwork === undefined ) {
+            selectTrainedNet();
+        }
     }
 
     render() {
@@ -19,9 +28,11 @@ class TestingView extends React.Component {
 }
 
 const mapStateToProps = (state, ownProps) => ({
-
+    currentNetwork: state.ui.currentNetwork
 })
 
-const mapDispatchToProps = null;
+const mapDispatchToProps = dispatch => ({
+    selectTrainedNet: () => dispatch(selectTrainedNetwork())
+})
 
-export default (connect(mapStateToProps, mapDispatchToProps)(TestingView));
+export default connect(mapStateToProps, mapDispatchToProps)(TestingView);
